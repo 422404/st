1689af70dcd94b0613fd46ffd303f6a762242440
422404 <elyanpoujol@gmail.com>
2020-10-31 23:45:49 +0100

    New bg color

--- config.def.h
+++ config.def.h
@@ -109,6 +109,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+    "#1a1a1a",
 };
 
 
@@ -117,7 +118,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
